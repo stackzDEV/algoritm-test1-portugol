bc60c5ddbcd875e729849916abc838ef309ba737
stackzDEV <90156451+stackzDEV@users.noreply.github.com>
2021-10-14 12:21:40 -0300

    Add files via upload
    refazendo calculadora básica (0.1)

---
+++ cauculadora.py
@@ -0,0 +1,11 @@
+# Começando a cauculadora
+
+n1 = int( input("Digite um  número : ") )
+n2 = int( input("Digite outro número : ") )
+
+soma = n1 + n2
+
+print('o resultado é :') 
+print('----------')
+print(       soma)
+print('----------')
